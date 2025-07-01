2514bc69fde1c149b9ff25cf7e358929d6898e3c
aevyy <wrk4avi@gmail.com>
2025-07-01 18:29:20 -0500

    Set up: Initial project skeleton
    Includes a submodule (my custom NeuralNetwork) and initial files yet to be implemented

---
+++ include/NeuroRF/SignalGenerator.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <complex>
+#include <string>
+#include <random>
+
+namespace NeuroRF {
+    enum ModulationType {
+
+    };
+
+    class SignalGenerator {
+
+    public:
+
+    private:
+
+    };
+}
---
+++ src/NeuroRF/SignalGenerator.cpp
@@ -0,0 +1,1 @@
+#include "..\..\include\NeuroRF\SignalGenerator.hpp"
