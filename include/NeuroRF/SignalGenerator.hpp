26839caddc957118f5dcc66ace8dc52310f4a3ce
aevyy <wrk4avi@gmail.com>
2025-07-03 20:13:10 -0500

    Add constructor for SignalGenerator
    -With random generator and random seed initialization
    - Preparing to add noise & SNR respectively

--- include/NeuroRF/SignalGenerator.hpp
+++ include/NeuroRF/SignalGenerator.hpp
@@ -9,15 +9,19 @@ namespace NeuroRF {
     enum ModulationType {
         BPSK,
         QPSK,
-        MOD_16_QAM
+        MOD_16_QAM  // Will look at this and additional types later on
     };
 
     class SignalGenerator {
     public:
+    SignalGenerator();
+
     std::complex<double> generateBPSK(int bit);
     std::complex<double> generateQPSK(int bit1, int bit2);
 
     private:
-
+    // Random components to generate noise
+    std::mt19937 generator;
+    std::random_device rd;
     };
 }
--- src/NeuroRF/SignalGenerator.cpp
+++ src/NeuroRF/SignalGenerator.cpp
@@ -1,5 +1,9 @@
 #include "..\..\include\NeuroRF\SignalGenerator.hpp"
 
+// Constructor: initializes the class with random generator & random seed
+NeuroRF::SignalGenerator::SignalGenerator() : generator(rd()) {}
+
+
 std::complex<double> NeuroRF::SignalGenerator::generateBPSK(int bit) {
     if (bit != 0 && bit != 1) {
         throw std::invalid_argument("Bit must be either 0 or 1");
@@ -9,23 +13,20 @@ std::complex<double> NeuroRF::SignalGenerator::generateBPSK(int bit) {
     else return std::complex<double>(-1.0, 0.0);
 }
 
+
 std::complex<double> NeuroRF::SignalGenerator::generateQPSK(int bit1, int bit2) {
     if ((bit1 != 0 && bit1 != 1) || (bit2 != 0 && bit2 != 1)) {
         throw std::invalid_argument("Bits must be either 0 or 1");
     }
-    
     if (bit1 == 0 && bit2 == 0) {
         return std::complex<double>(+1.0, +1.0);
     }
-
     else if (bit1 == 0 && bit2 == 1) {
         return std::complex<double>(-1.0, +1.0);
     }
-
     else if (bit1 == 1 && bit2 == 0) {
         return std::complex<double>(-1.0, -1.0);
     }
-
     else {
         return std::complex<double>(+1.0, -1.0);
     }
